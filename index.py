e996e5027ddbf94a87c3343e96a0a733c097cc9b
Mateo Epalza <mateoepalzaramirez@gmail.com>
2021-04-24 19:50:40 -0500

    front

--- index.py
+++ index.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/<int:edad>')
-def index(edad):
-    return render_template('index.html',edad=edad)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data/<int:numVariables>/<int:numRestricciones>')
+def data(numVariables, numRestricciones):
+    return render_template("data.html", restricciones = numRestricciones)
+
+
+@app.route('/getData', methods=['POST'])
+def getData():
+    print(request.form.get('MinMax'))
+    print(dict(request.form))
+    print(type(request.form))
+    return ""
 
 if __name__=="__main__":
     app.run(debug=True)
