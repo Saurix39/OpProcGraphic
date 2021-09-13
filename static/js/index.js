d1d1b8325ce2bd81d25ee6fbce3cb3893430fce3
Santiago Gonzalez <santiago.gonzalez@zinobe.com>
2021-09-12 22:45:58 -0500

    Update new method

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
+    validateMethod();
+})
 
-    // This event enable or disable the button
-    document.querySelector("#ecuaciones").onkeyup = function (){
-        if(this.value != null & this.value != ""){
-            document.querySelector("#btnSent").disabled = false
-        }else{
-            document.querySelector("#btnSent").disabled = true;
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".input-ecuaciones i:nth-child(2)").addEventListener("click", ()=>sumEcuaciones(true)); 
+    document.querySelector(".input-ecuaciones i:nth-child(3)").addEventListener("click", ()=>resEcuaciones(true));
+    
+    document.querySelector(".input-variables i:nth-child(2)").addEventListener("click", ()=>sumEcuaciones(false));
+    document.querySelector(".input-variables i:nth-child(3)").addEventListener("click", ()=>resEcuaciones(false));
+    
+    const select = document.querySelector("select");
+    select.value = "metodoGrafico";
+    select.addEventListener("click", validateMethod);
+    validateMethod();
+});
+
+
+const sumEcuaciones = (flag)=>{
+    let input;
+    if (flag) input =  document.getElementById("ecuaciones");
+    else input = document.getElementById("variables");
+
+    if (input.value == "") input.value = 1
+    else input.value = (parseInt(input.value) + 1);
+}
+
+const resEcuaciones = (flag)=>{
+    let input;
+    if (flag) input = document.getElementById("ecuaciones")
+    else input = document.getElementById("variables");
+
+    if (input.value == "" || input.value == 1) input.value = 1
+    else input.value = (parseInt(input.value) - 1);
+}
+
+const validateMethod = ()=>{
+    const method = document.querySelector("select").value;
+    const input = document.getElementById("variables");
+    const i = document.querySelectorAll(".input-variables i");
+    if (method == "metodoGrafico"){
+        input.value = 2;
+        for (element of i){
+            element.style.display = "none";
         }
     }
-});
+    else{
+        input.value = 1;
+        for (element of i){
+            element.style.display = "block";
+        }
+    }
+}
